3b812a0f2414c25c8cb4aadec29ef64f787108bc
SALAHEDDINE ALLOUCHE <sdalami123@gmail.com>
2023-12-12 11:59:07 +0100

    initialise

---
+++ customer-front-thymeleaf-app/src/main/java/ma/sdia/customerfrontthymeleafapp/CustomerFrontThymeleafAppApplication.java
@@ -0,0 +1,13 @@
+package ma.sdia.customerfrontthymeleafapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerFrontThymeleafAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerFrontThymeleafAppApplication.class, args);
+    }
+
+}
