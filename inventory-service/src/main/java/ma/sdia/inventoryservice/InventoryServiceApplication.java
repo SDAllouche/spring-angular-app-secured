e1a00f2179ca830f112c8fc65805912e9fb7114c
SALAHEDDINE ALLOUCHE <sdalami123@gmail.com>
2023-12-12 21:07:34 +0100

    update

--- inventory-service/src/main/java/ma/sdia/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/ma/sdia/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,36 @@
 package ma.sdia.inventoryservice;
 
+import ma.sdia.inventoryservice.entities.Product;
+import ma.sdia.inventoryservice.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Random;
+import java.util.UUID;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(ProductRepository productRepository){
+        return args -> {
+            Random quantity = new Random();
+            Random price = new Random();
+            for (int i=1;i<=10;i++) {
+                productRepository.save(Product.builder()
+                        .id(UUID.randomUUID().toString())
+                        .name("Product"+i)
+                        .quantity(quantity.nextInt(11))
+                        .price(price.nextInt(20)*1000)
+                        .build());
+            }
+        };
     }
 
 }
---
+++ inventory-service/src/main/java/ma/sdia/inventoryservice/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package ma.sdia.inventoryservice.repository;
+
+
+import ma.sdia.inventoryservice.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,String> {
+}
