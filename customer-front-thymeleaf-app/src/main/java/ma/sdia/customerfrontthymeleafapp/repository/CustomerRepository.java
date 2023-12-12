766609298d2ba627f90153f0ee887693e855d5f6
SALAHEDDINE ALLOUCHE <sdalami123@gmail.com>
2023-12-12 20:49:35 +0100

    update

--- customer-front-thymeleaf-app/src/main/java/ma/sdia/customerfrontthymeleafapp/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package ma.sdia.customerfrontthymeleafapp.repository;
-
-
-import ma.sdia.customerfrontthymeleafapp.entities.Customer ;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
