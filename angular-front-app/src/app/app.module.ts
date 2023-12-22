31d037337cceda7057ee8dced6fb7e3b2e7e01e2
SALAHEDDINE ALLOUCHE <sdalami123@gmail.com>
2023-12-22 21:37:32 +0100

    update

--- angular-front-app/src/app/app.module.ts
+++ angular-front-app/src/app/app.module.ts
@@ -12,7 +12,7 @@ function initializeKeycloak(keycloak: KeycloakService) {
   return () =>
     keycloak.init({
       config: {
-        url: 'http://localhost:8080',
+        url: 'http://0.0.0.0:8080',
         realm: 'sdia-realm',
         clientId: 'sdia-angular-client'
       },
